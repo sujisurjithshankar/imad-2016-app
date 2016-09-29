96902d200ad3c6540af7ce1565fcdabe6ec52380
sujisurjithshankar <sujisurjithshankar@gmail.com>
2016-09-29 21:25:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req,res){
-    res.send('Article will be presented here')
+    res.send('Article will be presented here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
