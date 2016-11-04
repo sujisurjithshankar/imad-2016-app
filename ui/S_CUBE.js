015e3d813e71ee660a68a028d4663dc0ed782b7c
sujisurjithshankar <sujisurjithshankar@gmail.com>
2016-11-04 10:53:23 +0530

    [imad-console] Updates ui/S_CUBE.js

--- ui/S_CUBE.js
+++ ui/S_CUBE.js
@@ -1,10 +1,4 @@
 function Now()
 {
-var d=new Date();
-var hours=d.getHours();
-var mins=d.getMinutes();
-var sec=d.getSeconds();
-var milli=d.getMilliseconds();
-document.body.innerHTML=hours+":"+mins+":"+sec+":"+milli;
-}
+
 setInterval(Now, 1);
